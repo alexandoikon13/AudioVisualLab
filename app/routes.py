416d4a1ab7710a1e14058b2dde83e4536bd34c55
alexandoikon13 <alexandoikon@gmail.com>
2023-11-21 16:14:34 +0100

    changed the code for processing

--- app/routes.py
+++ app/routes.py
@@ -1,40 +1,40 @@
-from flask import render_template, request, send_from_directory, send_file, jsonify, redirect, url_for
+from flask import render_template, request, send_from_directory, jsonify, redirect, url_for
 from werkzeug.utils import secure_filename  # Import secure_filename
-from scripts.audio_processing import load_audio, save_audio, crossfade, logarithmic_crossfade, smooth_edges
+from scripts.audio_processing import load_audio, save_audio, crossfade, smooth_edges
 import os
 
 def init_routes(app):
-
     @app.route('/')
     def index():
-        # return 'Welcome to the File Converter App'
         return redirect(url_for('upload'))
 
     @app.route('/upload')
     def upload():
         return render_template('upload.html')
 
-    @app.route('/handle_upload', methods=['POST'])  # Changed to a different endpoint
+    @app.route('/handle_upload', methods=['POST'])
     def handle_upload():
         file = request.files['file']
         action = request.form.get('action')
-
         filename = secure_filename(file.filename)
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(filepath)
 
-        # Process the file based on selected action
-        if action == 'crossfade':
-            # Call crossfade function
-            # ...
-            pass    #TO DELETE
-        elif action == 'smooth_edges':
-            # Call smooth_edges function
-            # ...
-            pass    #TO DELETE
+        audio, sr = load_audio(filepath)
+        processed_audio = None
 
-        processed_filepath = "path_to_processed_file"
-        return send_from_directory(app.config['UPLOAD_FOLDER'], processed_filepath)
+        if action == 'crossfade':
+            # Example: crossfade with itself for demonstration
+            processed_audio = crossfade(audio, audio, int(0.5 * sr))  # 0.5 seconds crossfade
+        elif action == 'smooth_edges':
+            processed_audio = smooth_edges(audio, int(0.1 * sr))  # 0.1 seconds smoothing at edges
+
+        processed_filename = 'processed_' + filename
+        processed_filepath = os.path.join(app.config['UPLOAD_FOLDER'], processed_filename)
+        save_audio(processed_audio, sr, processed_filepath)
+
+        return send_from_directory(app.config['UPLOAD_FOLDER'], processed_filename, as_attachment=True)
+
 
 ########################################################################
 #########################  START TODO ##################################       
@@ -62,16 +62,3 @@ def init_routes(app):
 
 #########################  END TODO ##################################
 ########################################################################   
-    @app.route('/convert', methods=['POST'])
-    def convert():
-        file = request.files['file']
-        target_format = request.form['format']
-        filename = secure_filename(file.filename)
-        input_path = os.path.join(UPLOAD_FOLDER, filename)
-        file.save(input_path)
-        
-        output_path = os.path.splitext(input_path)[0] + '.' + target_format
-        convert_audio_format(input_path, output_path)
-        
-        return send_file(output_path, as_attachment=True)
-
--- scripts/audio_processing.py
+++ scripts/audio_processing.py
@@ -11,73 +11,21 @@ def load_audio(file_path):
 def save_audio(audio, sample_rate, file_path):
     sf.write(file_path, audio, sample_rate)
 
-def convert_audio_format(input_path, output_path):
-    data, samplerate = sf.read(input_path)
-    sf.write(output_path, data, samplerate)
-
-# Crossfade function
+# Funtion to apply Crossfade between two audio segments
 def crossfade(audio1, audio2, crossfade_samples):
     fade_out = np.linspace(1, 0, crossfade_samples)
     fade_in = np.linspace(0, 1, crossfade_samples)
-
     overlap1 = audio1[-crossfade_samples:] * fade_out
     overlap2 = audio2[:crossfade_samples] * fade_in
-
     return np.concatenate((audio1[:-crossfade_samples], overlap1 + overlap2, audio2[crossfade_samples:]))
 
-def logarithmic_crossfade(audio1, audio2, crossfade_samples):
-    fade_out = np.logspace(0, -1, crossfade_samples)
-    fade_in = np.logspace(-1, 0, crossfade_samples)
-
-    overlap1 = audio1[-crossfade_samples:] * fade_out
-    overlap2 = audio2[:crossfade_samples] * fade_in
-
-    return np.concatenate((audio1[:-crossfade_samples], overlap1 + overlap2, audio2[crossfade_samples:]))
-
-# Smooth edges function
+# Function to Smooth the edges of an audio segment
 def smooth_edges(audio, edge_samples):
     if edge_samples > 0:
         fade_in = np.linspace(0, 1, edge_samples)
         fade_out = np.linspace(1, 0, edge_samples)
-
         audio[:edge_samples] *= fade_in
         audio[-edge_samples:] *= fade_out
-    
     return audio
 
-# Load the entire song
-audio, sr = load_audio('WhatsApp_Audio.mp3')
-
-# Specify the start and end time in seconds of the part to be removed
-start_time = 21.03
-end_time = 21.32
-
-# Convert time to samples
-start_sample = librosa.time_to_samples(start_time, sr=sr)
-end_sample = librosa.time_to_samples(end_time, sr=sr)
-
-# Cut the part out
-part1 = audio[:start_sample]
-part2 = audio[end_sample:]
-
-# Define crossfade duration in seconds (adjust as needed)
-crossfade_duration = (end_time - start_time)*0.943975  # crossfade_duration = 0.5
-crossfade_samples = librosa.time_to_samples(crossfade_duration, sr=sr)
-part1_normalized = librosa.util.normalize(part1)
-part2_normalized = librosa.util.normalize(part2)
-
-# Apply crossfade
-# audio_crossfaded = crossfade(part1, part2, crossfade_samples)
-# audio_crossfaded = crossfade(part1_normalized, part2_normalized, crossfade_samples)
-audio_crossfaded = logarithmic_crossfade(part1_normalized, part2_normalized, crossfade_samples)
-
-# Apply smoothing to the cut edges (adjust duration as needed)
-edge_smoothing_duration = (end_time - start_time)*0.15   # edge_smoothing_duration = 0.5  # seconds
-edge_samples = librosa.time_to_samples(edge_smoothing_duration, sr=sr)
-audio_smoothed = smooth_edges(audio_crossfaded, edge_samples)
-
-# Normalize the audio
-normalized_audio = librosa.util.normalize(audio_smoothed)
-
-# Save the final audio
-save_audio(normalized_audio, sr, 'final_song_normalized.wav')  # saving as WAV
+# Add other processing functions as needed
